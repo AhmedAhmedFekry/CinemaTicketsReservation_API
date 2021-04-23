1aa831512a201393f702d97655fd88b51c29149d
AhmedAhmedFekry <ahmedahmedfekry11305654@gmail.com>
2021-04-23 14:16:01 +0200

    without REST and no model query FBV

--- CinemaTicketsReservation_API/urls.py
+++ CinemaTicketsReservation_API/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from tickets import views
+
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     #1
+    path('django/jsonresponsenomodel/', views.no_rest_no_model),
 ]
